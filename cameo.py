0aa6d47ec8045b3de42bb0edb49484ee6ded06b0
zlp <779867650@qq.com>
2019-02-11 17:19:43 +0800

    First commit

--- cameo.py
+++ cameo.py
@@ -6,6 +6,7 @@
 # Date:         2019/2/6
 # -------------------------------------------------------------------------------
 import cv2
+import filters
 from managers import WindowManager, CaptureManager
 
 
@@ -13,13 +14,15 @@ class Cameo(object):
     def __init__(self):
         self._windowManager = WindowManager('Cameo', self.onKeypress)
         self._captureManager = CaptureManager(cv2.VideoCapture(0), self._windowManager, True)
+        self._curveFilter = filters.BGRPortraCurveFilter()
 
     def run(self):
         self._windowManager.createWindow()
         while self._windowManager.isWindowCreated:
             self._captureManager.enterFrame()
             frame = self._captureManager.frame
-            # TODO filter the frame
+            filters.strokeEdges(frame, frame)
+            self._curveFilter.apply(frame, frame)
             self._captureManager.exitFrame()
             self._windowManager.processEvents()
 
