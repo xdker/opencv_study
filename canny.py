867ceaa1bb1226cbf16ef8d7ebff88d3139218d7
zlp <779867650@qq.com>
2019-02-17 22:27:58 +0800

    chapter 3

---
+++ canny.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-#
+
+# -------------------------------------------------------------------------------
+# Name:         canny
+# Description:  
+# Author:       superman
+# Date:         2019/2/15
+# -------------------------------------------------------------------------------
+import cv2
+import numpy as np
+
+img = cv2.imread("./images/statue_small.jpg", 0)
+cv2.imwrite("canny.jpg", cv2.Canny(img, 200, 300))
+cv2.imshow("canny", cv2.imread("canny.jpg"))
+cv2.waitKey()
+cv2.destroyAllWindows()
