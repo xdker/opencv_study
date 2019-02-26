0079e365882957a51a3992ecb2fd18c0a99906e3
xdker <z779867650@gmail.com>
2019-02-26 21:15:26 +0800

    人脸检测练习

--- face_detection.py
+++ face_detection.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-#
 # -------------------------------------------------------------------------------
 # Name:         face_detection.py
-# Description:  
+# Description:  获取摄像头视频帧，
 # Author:       superman
 # Date:         2019/2/26
 # -------------------------------------------------------------------------------
@@ -12,7 +12,7 @@ def detect():
     face_cascade = cv2.CascadeClassifier("./cascades/haarcascade_frontalface_default.xml")
     eye_cascade = cv2.CascadeClassifier("./cascades/haarcascade_eye.xml")
     camera = cv2.VideoCapture(0)
-    while (True):
+    while (1):
         ret, frame = camera.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -21,12 +21,12 @@ def detect():
             roi_gray = gray[y:y + h, x:x + w]
             eyes = eye_cascade.detectMultiScale(roi_gray, 1.03, 5, 0, (40, 40))
             for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(img, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+                cv2.rectangle(img, (ex + x, ey + y), (ex + x + ew, ey + y + eh), (0, 255, 0), 2)
                 cv2.imshow("face_detection", frame)
                 if cv2.waitKey(1) & 0xff == ord("q"):
                     break
-            camera.release()
-            cv2.destroyAllWindows()
+                    camera.release()
+                    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
